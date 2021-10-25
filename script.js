616683e0ef01ac1caf6ded1c92f9def7ee07155b
Littl3S3al <lauseal@gmail.com>
2021-10-25 13:25:40 +0200

    made fonts

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ const frame = document.querySelector('.demo-2__gallery')
 
 frame.style.width = images.length * 100 + 'rem';
 
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        console.log('this is a project')
-    })
-})
